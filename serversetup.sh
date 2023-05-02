a76ba91d4f3ee2b8262061b4af6ff0e166ac1ebb
Hzqkii <128440086+Hzqkii@users.noreply.github.com>
2023-05-02 22:11:49 +1000

    Update serversetup.sh

--- serversetup.sh
+++ serversetup.sh
@@ -8,16 +8,16 @@ cd $HOME
 mkdir server
 cd server
 
-pv=1.19.4
-bn=522
+pv=1.19.4 #paper version
+bn=522 #paper build number
 wget https://api.papermc.io/v2/projects/paper/versions/${pv}/builds/${bn}/downloads/paper-${pv}-${bn}.jar
 
 mkdir plugins
 
 cd plugins
 
-vvv=4.6.2
-vbv=4.6.1
+vvv=4.6.2 #viaversion version
+vbv=4.6.1 #viabackwards version
 wget https://github.com/ViaVersion/ViaVersion/releases/download/${vvv}/ViaVersion-${vvv}.jar
 wget https://github.com/ViaVersion/ViaBackwards/releases/download/${vbv}/ViaBackwards-${vbv}.jar
 clear
@@ -27,4 +27,4 @@ cd ../
 wget https://raw.githubusercontent.com/Hzqkii/McServerAutoSetup/main/startserver.sh
 
 echo This script will now terminate itself you will need to edit the launch script for the server, use the command "nano $HOME/server/start.sh"
-echo change the "artefactname" to the name of the paper jar and change the 3 in "-Xmx3G and -Xmn3G" to what ever ram gb you want!
+echo Change the 3 in "-Xmx3G and -Xmn3G" to what ever ram you want to allocate to the server!
